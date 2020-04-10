cb7016edb99716d2eaa19aa83c9fe53b2ced9612
erictherobot <erictherobot@gmail.com>
2020-04-10 10:15:50 -0400

    Adding note about custom styles

--- src/routes/custom-themes.svelte
+++ src/routes/custom-themes.svelte
@@ -9,3 +9,18 @@
 <h2>Custom Themes</h2>
 
 <Prism language="javascript" code={`console.log('Coming soon', true) `} />
+
+<br />
+<h3>CURRENT STATUS</h3>
+<p>
+  You may have used our components and realized that you currently cannot
+  customize the component styles. We understand that this is a huge blocker for
+  everyone and we do apologize for the inconvenience. Please note that We have
+  every intention of making our component library the most simplistic and
+  intuitive for designers and developers alike.
+</p>
+<p>
+  While we're still in Alpha, we will be experiementing with theme customizers
+  and exposing new ways to access the theme properties of Svelteit components.
+  Thank you for your patience!
+</p>
